99ccaf0f545d625812ba436ea3b69b987ec6e865
mesakhbesta <mesakhbesta@gmail.com>
2025-10-11 23:58:27 +0700

    Update app_final.py

--- app_final.py
+++ app_final.py
@@ -941,7 +941,7 @@ def run_looker_page():
             horizontal=True,
             label_visibility="visible"
         )
-        iframe_height = 750 if "Terbuka" in view_mode else 1000
+        iframe_height = 850 if "Terbuka" in view_mode else 1000
 
     # === INFO CARD DENGAN AKSEN MERAH ===
     st.markdown("""
@@ -1216,4 +1216,5 @@ if page == "🎬 ReelTalk Analyzer":
 else:
 
     run_looker_page()
+
 
